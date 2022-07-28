a52f15f093ae2cd840fc0690cc2dc33ec6856038
Daniele Palmas <dnlplm@gmail.com>
2022-07-28 16:07:54 +0200

    drivers: bus: mhi: let userspace manage xfp fw update states
    After performing the firmware update in SBL through the XFP
    protocol, FN980 resets and is not detected anymore by the host.
    
    Add a new controller file to be used when performing the firmware update
    in order to:
    
    - Make the mhi stack aware of the process and not to reset the
    function during the update.
    - Force a reset of the mhi stack at the end of the process in order to
    detect the modem again.
    
    This file should be used both when performing the update through the USB
    composition 0x1055 (FN980 old hw revision) that through PCIe (both FN980
    and FN990).
    
    For PCIe the flow is the following:
    
    - Disable runtime power management on the mhi pci device
    - Write 1 to the fw_update sysfs file to races between
      the recovery work and other mhi operations
    - Start the firmware update
    - Write 0 to the fw_update sysfs file to reset the mhi stack
    
    Firmware update requires also upstream commit
    d651ce8e917fa1bf6cfab8dca74c512edffc35d3
    bus: mhi: core: Fix race while handling SYS_ERR at power up
    
    Signed-off-by: Daniele Palmas <dnlplm@gmail.com>
    (cherry picked from commit 4b0048e17fd78862e9a8babf38bfaeebcab5c30d)

--- drivers/bus/mhi/host/pci_generic.c
+++ drivers/bus/mhi/host/pci_generic.c
@@ -22,7 +22,7 @@
 
 #define MHI_POST_RESET_DELAY_MS 2000
 
-#define HEALTH_CHECK_PERIOD (HZ * 2)
+#define HEALTH_CHECK_PERIOD (HZ / 2)
 
 /* PCI VID definitions */
 #define PCI_VENDOR_ID_THALES	0x1269
@@ -1026,6 +1026,19 @@ static void health_check(struct timer_list *t)
 	if (!test_bit(MHI_PCI_DEV_STARTED, &mhi_pdev->status) ||
 			test_bit(MHI_PCI_DEV_SUSPENDED, &mhi_pdev->status))
 		return;
+
+	if (mhi_cntrl->xfp == XFP_STATE_FLASHING) {
+		/* Flashing through USB, wait until users notifies that it has finished */
+		mod_timer(&mhi_pdev->health_check_timer, jiffies + HEALTH_CHECK_PERIOD);
+		return;
+	}
+
+	if (mhi_cntrl->xfp == XFP_STATE_NEED_RESET) {
+		mhi_cntrl->xfp = XFP_STATE_IDLE;
+		dev_dbg(mhi_cntrl->cntrl_dev, "Device needs to be resetted EE = %d\n", mhi_cntrl->ee);
+		queue_work(system_long_wq, &mhi_pdev->recovery_work);
+		return;
+	}
 
 	if (!mhi_pci_is_alive(mhi_cntrl)) {
 		dev_err(mhi_cntrl->cntrl_dev, "Device died\n");
--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -288,6 +288,18 @@ struct mhi_controller_config {
 };
 
 /**
+ * enum xfp_state - xfp flashing state
+ * @XFP_STATE_IDLE: not flashing
+ * @XFP_STATE_FLASHING: flashing
+ * @XFP_STATE_NEED_RESET: mhi stack needs to be resetted
+ */
+enum xfp_state {
+	XFP_STATE_IDLE = 0x0,
+	XFP_STATE_FLASHING = 0x1,
+	XFP_STATE_NEED_RESET = 0x2,
+};
+
+/**
  * struct mhi_controller - Master MHI controller structure
  * @name: Device name of the MHI controller
  * @cntrl_dev: Pointer to the struct device of physical bus acting as the MHI
@@ -360,6 +372,7 @@ struct mhi_controller_config {
  * @fbc_download: MHI host needs to do complete image transfer (optional)
  * @wake_set: Device wakeup set flag
  * @irq_flags: irq flags passed to request_irq (optional)
+ * @xfp: xfp state used for flashing
  * @mru: the default MRU for the MHI device
  *
  * Fields marked as (required) need to be populated by the controller driver
@@ -446,6 +459,10 @@ struct mhi_controller {
 	bool wake_set;
 	unsigned long irq_flags;
 	u32 mru;
+	/* Flag to manage flashing status  */
+	enum xfp_state xfp;
+	/* Flag to detect forced SBL */
+	bool forced_SBL;
 };
 
 /**
