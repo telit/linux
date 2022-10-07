f87601c721b58505cd11c396e3e8b2bacbff8f8a
Daniele Palmas <dnlplm@gmail.com>
2022-10-07 14:02:29 +0200

    bus: mhi: host: add support for xfp flashing
    xfp flashing requires health check to be disabled during firmware
    update and the mhi stack to be resetted after firmware upgrade.
    
    This process should be controlled by userspace, so provide a
    sysfs file for this task.
    
    The firmware update process consists of these steps:
    
    - Disable runtime power management on the mhi pci device
    - Write 1 to the fw_update sysfs file
    - Start the firmware update
    - Write 0 to the fw_update sysfs file to reset the mhi stack
    
    Signed-off-by: Daniele Palmas <dnlplm@gmail.com>

--- drivers/bus/mhi/host/pci_generic.c
+++ drivers/bus/mhi/host/pci_generic.c
@@ -849,6 +849,19 @@ static void health_check(struct timer_list *t)
 			test_bit(MHI_PCI_DEV_SUSPENDED, &mhi_pdev->status))
 		return;
 
+	if (mhi_cntrl->xfp == XFP_STATE_FLASHING) {
+		/* Flashing through USB, wait until users notifies that it has finished */
+		mod_timer(&mhi_pdev->health_check_timer, jiffies + HEALTH_CHECK_PERIOD);
+		return;
+	}
+
+	if (mhi_cntrl->xfp == XFP_STATE_NEED_RESET) {
+		mhi_cntrl->xfp = XFP_STATE_IDLE;
+		dev_dbg(mhi_cntrl->cntrl_dev, "Device needs to be resetted EE = %d\n", mhi_cntrl->ee);
+		queue_work(system_long_wq, &mhi_pdev->recovery_work);
+		return;
+	}
+
 	if (!mhi_pci_is_alive(mhi_cntrl)) {
 		dev_err(mhi_cntrl->cntrl_dev, "Device died\n");
 		queue_work(system_long_wq, &mhi_pdev->recovery_work);
--- include/linux/mhi.h
+++ include/linux/mhi.h
@@ -287,6 +287,18 @@ struct mhi_controller_config {
 };
 
 /**
+ * enum xfp_state - xfp flashing state
+ * @XFP_STATE_IDLE: not flashing
+ * @XFP_STATE_FLASHING: flashing
+ * @XFP_STATE_NEED_RESET: mhi stack needs to be resetted
+ */
+enum xfp_state {
+	XFP_STATE_IDLE = 0x0,
+	XFP_STATE_FLASHING = 0x1,
+	XFP_STATE_NEED_RESET = 0x2,
+};
+
+/**
  * struct mhi_controller - Master MHI controller structure
  * @cntrl_dev: Pointer to the struct device of physical bus acting as the MHI
  *            controller (required)
@@ -358,6 +370,7 @@ struct mhi_controller_config {
  * @wake_set: Device wakeup set flag
  * @irq_flags: irq flags passed to request_irq (optional)
  * @mru: the default MRU for the MHI device
+ * @xfp: xfp state used for flashing
  *
  * Fields marked as (required) need to be populated by the controller driver
  * before calling mhi_register_controller(). For the fields marked as (optional)
@@ -452,6 +465,8 @@ struct mhi_controller {
 	bool wake_set;
 	unsigned long irq_flags;
 	u32 mru;
+	/* Flag to manage flashing status  */
+	enum xfp_state xfp;
 	/* Flag to detect forced SBL */
 	bool forced_SBL;
 };
